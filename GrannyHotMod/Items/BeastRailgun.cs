1330746e2bfe8496f433ab99b08e4be9c0d69817
PinkGrannyBeast <50644102+PinkGrannyBeast@users.noreply.github.com>
2019-06-03 11:45:15 -0700

    Add files via upload

---
+++ GrannyHotMod/Items/BeastRailgun.cs
@@ -0,0 +1,61 @@
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+namespace GrannyHotMod.Items
+{
+ public class BeastRailgun : ModItem
+ {
+  public override void SetStaticDefaults()
+    	{
+        	DisplayName.SetDefault("Beast Railgun");
+        	Tooltip.SetDefault("Railgun firing the energy of Beast");
+    	}
+    	
+  public override void SetDefaults()
+    	{
+   item.damage = 30;
+   item.ranged = true;
+   item.width = 40;
+   item.height = 40;
+   item.useTime = 4;
+   item.useAnimation = 20;
+   item.useStyle = 5;
+   item.noMelee = true;
+   item.knockBack = 4;
+   item.value = 10000;
+   item.rare = 8;
+   item.UseSound = SoundID.Item11;
+   item.autoReuse = true;
+   item.shoot = 10;
+   item.shootSpeed = 6f;
+   item.useAmmo = AmmoID.Bullet;
+  }
+		public override void AddRecipes() 
+        {
+			ModRecipe recipe = new ModRecipe(mod);
+            recipe.AddIngredient(null, "LesserHealingPotionBar", 10);
+            recipe.AddIngredient(null, "DirtBar", 10);
+            recipe.AddIngredient(null, "BeastSauce", 18);
+			recipe.AddTile(TileID.WorkBenches);
+			recipe.SetResult(this);
+			recipe.AddRecipe();
+		}
+    	
+    	public override bool Shoot(Player player, ref Vector2 position, ref float speedX, ref float speedY, ref int type, ref int damage, ref float knockBack)
+  {
+        	Vector2 perturbedSpeed = new Vector2(speedX, speedY).RotatedByRandom(MathHelper.ToRadians(6));
+        	speedX = perturbedSpeed.X;
+        	speedY = perturbedSpeed.Y;
+        	Projectile.NewProjectile(position.X, position.Y, speedX, speedY, 126, damage, knockBack, player.whoAmI);
+        	return false;
+  }
+  
+    	
+    	public override Vector2? HoldoutOffset()
+  {
+   return new Vector2(-33, 0);
+  }
+ }
+}
+
---
+++ GrannyHotMod/Items/BeastWand.cs
@@ -0,0 +1,44 @@
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace GrannyHotMod.Items
+{
+	public class BeastWand : ModItem
+	{
+		public override void SetStaticDefaults() {
+			Tooltip.SetDefault("The might of Beast in Magic");
+			Item.staff[item.type] = true;
+		}
+
+		public override void SetDefaults() {
+			item.damage = 70;
+			item.magic = true;
+			item.mana = 4;
+			item.width = 70;
+			item.height = 70;
+			item.useTime = 5;
+			item.useAnimation = 5;
+			item.useStyle = 5;
+			item.noMelee = true;
+			item.knockBack = 5;
+			item.value = 10000;
+			item.rare = 11;
+			item.UseSound = SoundID.Item20;
+			item.autoReuse = true;
+			item.shoot = 618;
+			item.shootSpeed = 16f;
+		}
+
+		public override void AddRecipes() 
+        {
+			ModRecipe recipe = new ModRecipe(mod);
+			recipe.AddIngredient(ItemID.Wood, 10);
+            recipe.AddIngredient(null, "BeastSauce", 16);
+			recipe.AddTile(TileID.WorkBenches);
+			recipe.SetResult(this);
+			recipe.AddRecipe();
+		}
+        
+	}
+}
